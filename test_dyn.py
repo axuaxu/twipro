b5b3783cef5074d50cce9dbfbca2166038079eca
EC2 Default User <ec2-user@ip-172-31-17-151.ec2.internal>
2017-09-08 18:20:08 +0000

    3

--- test_dyn.py
+++ test_dyn.py
@@ -1,40 +1,38 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 
-# Create the DynamoDB table.
-table = dynamodb.create_table(
-    TableName='users',
-    KeySchema=[
-        {
-            'AttributeName': 'username',
-            'KeyType': 'HASH'
-        },
-        {
-            'AttributeName': 'last_name',
-            'KeyType': 'RANGE'
-        }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'username',
-            'AttributeType': 'S'
-        },
-        {
-            'AttributeName': 'last_name',
-            'AttributeType': 'S'
-        },
-
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
-
-# Wait until the table exists.
-table.meta.client.get_waiter('table_exists').wait(TableName='users')
+# Instantiate a table resource object without actually
+# creating a DynamoDB table. Note that the attributes of this table
+# are lazy-loaded: a request is not made nor are the attribute
+# values populated until the attributes
+# on the table resource are accessed or its load() method is called.
+table = dynamodb.Table('imglist')
 
 # Print out some data about the table.
-print(table.item_count)
+# This will cause a request to be made to DynamoDB and its attribute
+# values will be set based on the response.
+#print(table.creation_date_time)
+#f= open('imglist.txt','r')
+#for line in f:
+#  table.put_item(
+#     Item={
+#        'name': line,
+#        }
+#      )
+#response = table.get_item(
+#    Key={
+#        'name': 'janedoe',
+#    }
+#)
+#item = response['Item']
+#print(item)
+
+response = table.scan(
+    FilterExpression=Attr('name').begins_with('images')
+)
+items = response['Items'][10]
+print(items)
+print(len(response['Items']))
