3541e1deb879b9571989a9123ef4c24735732fe3
EC2 Default User <ec2-user@ip-172-31-17-151.ec2.internal>
2017-09-10 19:27:54 +0000

    3

--- test_event.py
+++ test_event.py
@@ -1,14 +1,30 @@
 import boto3
+import datetime
+
+print datetime.datetime.now()
 
 
 # Create CloudWatchEvents client
 cloudwatch_events = boto3.client('events')
 
-# Put an event rule
+# Put an event rule   "cron(0 20 * * ? *)"
 response = cloudwatch_events.put_rule(
     Name='DEMO_EVENT',
     RoleArn='arn:aws:iam::969340917686:role/axevent1',
     ScheduleExpression='rate(5 minutes)',
     State='ENABLED'
 )
+
+# Put target for rule
+response = cloudwatch_events.put_targets(
+    Rule='DEMO_EVENT',
+    Targets=[
+        {
+            'Arn': 'LAMBDA_FUNCTION_ARN',
+            'Id': 'myCloudWatchEventsTarget',
+        }
+    ]
+)
+print(response)
+
 print(response['RuleArn'])
