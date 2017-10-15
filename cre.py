8178189488d43d5cdefe1473c7a10cfbfdd4aea0
axuaxu <axu0110@gmail.com>
2017-10-15 15:12:02 -0400

    Delete cre.py

--- cre.py
+++
@@ -1,5 +0,0 @@
-#https://www.digitalocean.com/community/tutorials/how-to-create-a-twitterbot-with-python-3-and-the-tweepy-library
-consumer_key='rD7LSDlI4TF3Iw1p9Q9xQ3lCh'
-consumer_secret ='pMvAJIUAxMzDyZjKEqHSo1k7qEHyo4sHmYTdEmAWTDplq4Zkfu'
-access_token= '899308310461075457-LxqERAtAfzgWbXHjT1k65MDsntNgxqz'
-access_token_secret= 'IMz3fbb0eop316Wid7JaHvLSfNnzcqRiv4Et9qlv5F56M'
